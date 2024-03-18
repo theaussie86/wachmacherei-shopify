4e76e5c99f55f6b94a54de6737a0c66d0ae4fcc6
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-03-18 06:29:13 +0100

    added Posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 import { CookieProvider } from 'lib/context/cookies';
+import { PHProvider } from 'lib/context/posthog';
 import ReCaptchaProvider from 'lib/context/recaptcha';
 import { PropsWithChildren } from 'react';
 
 function Providers({ children }: PropsWithChildren) {
   return (
     <ReCaptchaProvider lng="de">
-      <CookieProvider>{children}</CookieProvider>
+      <CookieProvider>
+        <PHProvider>{children}</PHProvider>
+      </CookieProvider>
     </ReCaptchaProvider>
   );
 }
